22e7adbacb2cf42f9f0c60db1a80855cc1d3e6fd
pragnasandireddy <sandireddypragna.18@gmail.com>
2023-01-04 15:04:57 +0530

    ok

---
+++ events/jq.js
@@ -0,0 +1,37 @@
+//writing with js including onclick fun1() in button tag
+/*function fun1(){
+    syntax : document.getElementById("img1").style.width="200px"
+}*/
+
+
+
+//writing function directly using 
+
+ /*$("button").click(fun1);
+ function fun1(){
+    $("#img1").css('width','200px')
+ }*/
+
+ //writing in less code
+/*$("button").click(function(){
+    $("#img1").css('width','200px');
+})*/
+
+//its perfect js file we use ready event
+$("document").ready(function(){
+    $(":button").click(function(){//we can also use dblclick
+        $("#img1").css('width','200px');
+    })
+})
+
+
+/*$("document").ready(function(){
+    $("#img1").mouseenter(function(){
+        $("#img1").css('width','200px');
+    })
+    $("#img1").mouseleave(function(){
+        $("#img1").css('width','200px');
+    })
+})
+*/
+
---
+++ jq2/jq.js
@@ -0,0 +1,13 @@
+//changing of colour using div tag selector ,onclick fun
+function fun2(){
+    //$("div").css("background-color","orange");//using element selector
+    //$("#p1").css("font-style","italic");// using id selector we can change single line
+    //$(".para").css("font-style","italic");//using class selector we can access different elements
+    //$("#div1,#div3").css("background-color","yellow");//using id selector accessing diff elements
+    //$("#div1,li").css("background-color","yellow");//using id selector and element selector same time 
+    //$("div>p").fadeToggle();// it removes all the element having div-tag parent and p-tag child
+    //$("p:first").fadeToggle();//first line fades
+    //$("p:second").fadeToggle();//second line fades
+    //$("li:even").fadeToggle();//even in list evement  fades
+    $("li:odd").fadeToggle();//odd in list evement  fades
+} 
