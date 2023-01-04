47879b9bf042a33c8d975adf0ebf5b90ee91c686
pragnasandireddy <sandireddypragna.18@gmail.com>
2023-01-04 14:52:51 +0530

    okie

---
+++ jquery/jq.js
@@ -0,0 +1,7 @@
+//function to fade out onclick
+function fun1(){
+    //syntax : $("element name").action() //jquery
+    //syntax : document.getElementById(element name)
+   // $("#img1").fadeToggle(); //$ its a selector to access html element
+    $("#img1").fadeToggle(3000);
+}
